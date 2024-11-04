c0fa53dff17920c0080d3d09b5991a69159e6a4a
mihael.golob <golob.mihael@gmail.com>
2024-11-04 18:18:16 +0100

    NEW: build with opencv, depthai-core-example now works woop woop

--- main.cpp
+++ main.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
 #include <depthai/depthai.hpp>
+#include <opencv2/opencv.hpp>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    
+    // Create pipeline
     dai::Pipeline pipeline;
+    auto colorCam = pipeline.create<dai::node::ColorCamera>();
+    auto xlinkOut = pipeline.create<dai::node::XLinkOut>();
+    xlinkOut->setStreamName("preview");
+    colorCam->setInterleaved(true);
+    colorCam->preview.link(xlinkOut->input);
+
+    try {
+        // Try connecting to device and start the pipeline
+        dai::Device device(pipeline);
+        // Get output queue
+        auto preview = device.getOutputQueue("preview");
+
+        cv::Mat frame;
+        while (true) {
+
+            // Receive 'preview' frame from device
+            auto imgFrame = preview->get<dai::ImgFrame>();
+
+            // Show the received 'preview' frame
+            cv::imshow("preview", cv::Mat(imgFrame->getHeight(), imgFrame->getWidth(), CV_8UC3, imgFrame->getData().data()));
+
+            // Wait and check if 'q' pressed
+            if (cv::waitKey(1) == 'q') return 0;
+
+        }
+    } catch (const std::runtime_error& err) {
+        std::cout << err.what() << std::endl;
+    } 
     
     return 0;
 }
