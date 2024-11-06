6a97e232da2b12ab1ed2d28c1950202cc844a402
mihael.golob <golob.mihael@gmail.com>
2024-11-06 06:58:31 +0100

    TEST: use nn with open vino to detect objects

--- main.cpp
+++ main.cpp
@@ -1,91 +1,74 @@
-#include <iostream>
-#include <opencv2/opencv.hpp>
 #include <depthai/depthai.hpp>
-#include <pcl/pcl_base.h>
-#include <pcl/visualization/pcl_visualizer.h>
+#include <boost/algorithm/clamp.hpp>
+#include <opencv2/opencv.hpp>
+
+constexpr auto rgb_str_ = "rgb";
+constexpr auto nn_str_ = "nn";
+
+cv::Rect frameNorm(const cv::Mat& frame, const std::vector<float>& bbox) {
+    int width = frame.cols;
+    int height = frame.rows;
+    
+    int x1 = static_cast<int>(boost::algorithm::clamp(bbox[0], 0.0f, 1.0f) * width);
+    int y1 = static_cast<int>(boost::algorithm::clamp(bbox[1], 0.0f, 1.0f) * height);
+    int x2 = static_cast<int>(boost::algorithm::clamp(bbox[2], 0.0f, 1.0f) * width);
+    int y2 = static_cast<int>(boost::algorithm::clamp(bbox[3], 0.0f, 1.0f) * height);
+    
+    return {x1, y1, x2 - x1, y2 - y1};
+}
 
 int main() {
-    auto pipeline = dai::Pipeline();
-    auto monoLeft = pipeline.create<dai::node::MonoCamera>();
-    auto monoRight = pipeline.create<dai::node::MonoCamera>();
-    auto depth = pipeline.create<dai::node::StereoDepth>();
-    auto pointcloud = pipeline.create<dai::node::PointCloud>();
-    auto xout = pipeline.create<dai::node::XLinkOut>();
-    auto xoutDepth = pipeline.create<dai::node::XLinkOut>();
-
-    monoLeft->setResolution(dai::MonoCameraProperties::SensorResolution::THE_400_P);
-    monoLeft->setCamera("left");
-    monoRight->setResolution(dai::MonoCameraProperties::SensorResolution::THE_400_P);
-    monoRight->setCamera("right");
-
-    // Create a node that will produce the depth map (using disparity output as
-    // it's easier to visualize depth this way)
-    depth->setDefaultProfilePreset(dai::node::StereoDepth::PresetMode::HIGH_DENSITY);
-    // Options: MEDIAN_OFF, KERNEL_3x3, KERNEL_5x5, KERNEL_7x7 (default)
-    depth->initialConfig.setMedianFilter(dai::MedianFilter::KERNEL_7x7);
-    depth->setLeftRightCheck(true);
-    depth->setExtendedDisparity(false);
-    depth->setSubpixel(true);
-
-    xout->setStreamName("out");
-    xoutDepth->setStreamName("depth");
-
-    monoLeft->out.link(depth->left);
-    monoRight->out.link(depth->right);
-    depth->depth.link(pointcloud->inputDepth);
-    depth->disparity.link(xoutDepth->input);
-    pointcloud->outputPointCloud.link(xout->input);
-    pointcloud->initialConfig.setSparse(true);
-
-    auto viewer = std::make_unique<pcl::visualization::PCLVisualizer>("Cloud Viewer");
-    bool first = true;
-
+    dai::Pipeline pipeline;
+    // nodes
+    auto cam_rgb = pipeline.create<dai::node::ColorCamera>();
+    auto detection_nn = pipeline.create<dai::node::MobileNetDetectionNetwork>();
+    
+    // outputs
+    auto rgb_out = pipeline.create<dai::node::XLinkOut>();
+    rgb_out->setStreamName(rgb_str_);
+    auto nn_out = pipeline.create<dai::node::XLinkOut>();
+    nn_out->setStreamName(nn_str_);
+    
+    // properties
+    cam_rgb->setBoardSocket(dai::CameraBoardSocket::CAM_A);
+    cam_rgb->setInterleaved(false);
+    cam_rgb->setPreviewSize(300, 300);
+    detection_nn->setBlobPath("model.blob");
+    detection_nn->setConfidenceThreshold(0.5);
+    
+    // linking
+    cam_rgb->preview.link(rgb_out->input);
+    cam_rgb->preview.link(detection_nn->input);
+    detection_nn->out.link(nn_out->input);
+    
+    // connect to device & start pipeline
     dai::Device device(pipeline);
-
-    auto q = device.getOutputQueue("out", 8, false);
-    auto qDepth = device.getOutputQueue("depth", 8, false);
-    long counter = 0;
-    while(true) {
-        std::cout << "Waiting for data" << std::endl;
-        auto depthImg = qDepth->get<dai::ImgFrame>();
-        auto pclMsg = q->get<dai::PointCloudData>();
-        std::cout << "Got data" << std::endl;
-        if(!pclMsg) {
-            std::cout << "No data" << std::endl;
-            continue;
+    auto rgb_queue = device.getOutputQueue(rgb_str_, 8, false);
+    auto nn_queue = device.getOutputQueue(nn_str_);
+    
+    cv::Mat rgb_frame;
+    std::vector<dai::ImgDetection> detections;
+   
+    while (true) {
+        const auto in_rgb = rgb_queue->tryGet<dai::ImgFrame>();
+        const auto in_nn = nn_queue->tryGet<dai::ImgDetections>();
+        
+        if (in_rgb) rgb_frame = in_rgb->getCvFrame();
+        if (in_nn) detections = in_nn->detections;
+        
+        if (rgb_frame.rows > 0 && rgb_frame.cols > 0) {
+            for (const auto& d : detections) {
+                auto bbox = frameNorm(rgb_frame, {d.xmin, d.ymin, d.xmax, d.ymax});
+                cv::rectangle(rgb_frame, bbox, cv::Scalar(255, 0, 0), 2);
+            }
+            
+            cv::imshow("preview", rgb_frame);
         }
-
-        auto frame = depthImg->getCvFrame();
-        frame.convertTo(frame, CV_8UC1, 255 / depth->initialConfig.getMaxDisparity());
-        cv::imshow("depth", frame);
-        cv::waitKey(1);
-
-        if(pclMsg->getPoints().empty()) {
-            std::cout << "Empty point cloud" << std::endl;
-            continue;
-        }
-        std::cout << "Number of points: " << pclMsg->getPoints().size() << std::endl;
-        std::cout << "Min x: " << pclMsg->getMinX() << std::endl;
-        std::cout << "Min y: " << pclMsg->getMinY() << std::endl;
-        std::cout << "Min z: " << pclMsg->getMinZ() << std::endl;
-        std::cout << "Max x: " << pclMsg->getMaxX() << std::endl;
-        std::cout << "Max y: " << pclMsg->getMaxY() << std::endl;
-        std::cout << "Max z: " << pclMsg->getMaxZ() << std::endl;
-
-//        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = pclMsg->getPclData();
-//        if(first) {
-//            viewer->addPointCloud<pcl::PointXYZ>(cloud, "cloud");
-//            first = false;
-//        } else {
-//            viewer->updatePointCloud(cloud, "cloud");
-//        }
-
-        viewer->spinOnce(10);
-
-        if(viewer->wasStopped()) {
-            break;
+        
+        if (cv::waitKey(1) == 'q') {
+            return 0;
         }
     }
-
+    
     return 0;
 }
